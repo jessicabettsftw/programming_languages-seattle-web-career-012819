5e8474daaf0979f05b0106dbc13eda3136c401cc
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-17 04:29:58 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
       type_info.each do |type_label, type|
         #puts "#{name}, #{type}, #{style}"
         if reformated.keys.include? name
-          reformated[name][:type][:style].push(style)
+          puts [name][:type][:style]
         else
         reformated[name] = {:type => type, :style => [style]}
         end
