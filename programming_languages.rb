7e10e357411f38b18ccecde566067ef5d4933aff
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-17 04:16:00 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,8 @@
 def reformat_languages(languages)
   reformated = {}
   languages.each do |style, language_info|
-    
+    language_info.each do |name, type_info|
+      type_info.each do |type_label, type|
+        puts "#{name}, #{type}, #{style}"
   end
 end
